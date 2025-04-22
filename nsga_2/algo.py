8dd6f85ac9472895b0aa3532de8380d62d919090
Cameron <cameronwood858@googlemail.com>
2025-04-22 12:43:45 +0100

    Refactor algo.py to improve function definitions and add objective function

--- nsga_2/algo.py
+++ nsga_2/algo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Iterable, Callable
 
 
 def create_population(size: int, bounds: tuple) -> np.ndarray:
@@ -19,3 +20,22 @@ def create_population(size: int, bounds: tuple) -> np.ndarray:
     """
     """"""
     return np.random.uniform(bounds[0], bounds[1], size=(size, 1))
+
+
+def objective(population: np.ndarray, funcs: Iterable[Callable]) -> np.ndarray:
+    """
+    Compute the objective values for the given population and functions.
+
+    Parameters
+    ----------
+    population : np.ndarray
+        The current population with shape (size, n_variables).
+    funcs : Iterable[Callable]
+        A list of functions that take the population as input and return an array of objective values.
+
+    Returns
+    -------
+    np.ndarray
+        A numpy array of shape (size, n_objectives) containing the objective values for each individual in the population.
+    """
+    return np.hstack([f(population) for f in funcs])
